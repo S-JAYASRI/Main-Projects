35d76329368b31a53d0bb820c612e85ff390cad7
S-JAYASRI <jsridinesh@gmail.com>
2024-09-19 13:14:48 +0530

    script file created

---
+++ Color-Flipper/script.js
@@ -0,0 +1,9 @@
+let color = document.getElementById("color");
+let wrap = document.getElementById("wrap");
+let button = document.getElementById("button");
+// hex decimal values divide 16 
+let hexVal = [1,2,3,4,5,6,7,8,9,"A","B","c","D","E","F"];
+
+
+
+
